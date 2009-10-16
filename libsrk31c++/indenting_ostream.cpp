16fe476607824ef27b5f18e0e83d017ca70babe4
srk31@font.cl.cam.ac.uk <srk31@font.cl.cam.ac.uk>
2009-10-16 17:11:22 +0100

    cake: more bugfixing on override/declare. Added CloneableTree for creating modified versions of value descriptions. bdwarf/libdwarfpp: major refactoring into spec, lib and encap. libsrk31c++: created library, consisting of indenting ostream implementation. libdwarfpp/tests: created (not fully working yet).

---
+++ libsrk31c++/indenting_ostream.cpp
@@ -0,0 +1,40 @@
+#include "indenting_ostream.hpp"
+
+namespace srk31
+{
+	struct newline_tabbing_filter : boost::iostreams::output_filter 
+	{
+		int *indent_level;
+		newline_tabbing_filter(int *plevel) : indent_level(plevel) {}
+    	template<typename Sink>
+    	bool put_char(Sink& dest, int c)
+    	{
+        	if (!boost::iostreams::put(dest, c)) return false;
+        	if (c == '\n')
+			{
+				for (int i = *indent_level; i > 0; i--) 
+				{
+					if (!boost::iostreams::put(dest, '\t')) return false;
+				}
+			}
+        	return true;
+    	}
+		template<typename Sink>
+    	bool put(Sink& dest, int c) 
+    	{
+        	return put_char(dest, c);
+    	}
+	};
+	
+	indenting_ostream::indenting_ostream(std::ostream& s) : 
+		std::ostream(p_wrapped_streambuf = new boost::iostreams::filtering_ostreambuf),
+		indent_level(0),
+		p_stream_filter(new newline_tabbing_filter(&indent_level))
+	{
+		p_wrapped_streambuf->push(*p_stream_filter);
+		p_wrapped_streambuf->push(s);
+	}
+	indenting_ostream::~indenting_ostream() { delete p_stream_filter; delete p_wrapped_streambuf; }
+
+	indenting_ostream indenting_cout(std::cout), indenting_cerr(std::cerr);
+}
---
+++ libsrk31c++/indenting_ostream.hpp
@@ -0,0 +1,39 @@
+#ifndef __SRK31_INDENTING_OSTREAM_HPP
+#define __SRK31_INDENTING_OSTREAM_HPP
+
+#include <map>
+#include <vector>
+#include <stack>
+#include <string>
+#include <memory>
+#include <iostream>
+#include <cassert>
+#include <boost/iostreams/concepts.hpp>    // input_filter
+#include <boost/iostreams/operations.hpp>  // get()
+#include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
+#include <boost/iostreams/filtering_streambuf.hpp>
+
+//namespace boost { namespace iostreams { class filtering_ostreambuf; } }
+
+namespace srk31 {
+	struct newline_tabbing_filter;
+	class indenting_ostream : public std::ostream
+	{
+		int indent_level;
+		newline_tabbing_filter *p_stream_filter;
+		boost::iostreams::filtering_ostreambuf *p_wrapped_streambuf;
+	public:
+		indenting_ostream(std::ostream& s = std::cout); 
+		virtual ~indenting_ostream();
+		
+		int inc_level() { indent_level++; *this << '\t'; return indent_level; }
+		int dec_level() { indent_level--; *this << "\b\b\b\b\b\b\b\b"; return indent_level; }
+		int level() { return indent_level; }
+
+	}; 
+
+	extern indenting_ostream indenting_cout, indenting_cerr;
+}
+
+#endif
