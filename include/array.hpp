8cb391ed981db6fab656aeb2a0f98ff455201d1f
Stephen Kell <srk31@cl.cam.ac.uk>
2012-03-09 21:53:02 +0000

    array.hpp: added array_wrapper for lvalues.

--- include/array.hpp
+++ include/array.hpp
@@ -41,16 +41,58 @@ public:
 	//operator El const* () const { return &a[0]; }
 };
 
+/* This is similar, but doesn't include any storage of its own. Rather,
+ * it provides an interface around some preexisting array. It's necessary
+ * when you want to assign to an array from an srk31::array or another array. */
+template <typename El, int Dim>
+class array_wrapper
+{
+	typedef El wrapped_type[Dim];
+	
+	wrapped_type& storage;
+	
+	typedef El raw_array[Dim];
+	typedef raw_array& raw_array_ref;
+	typedef raw_array const& raw_array_const_ref;
+public:
+	El& operator[](int ind) { return storage[ind]; }
+	const El& operator[](int ind) const { return storage[ind]; }
+	
+	// constructors -- unlike array, these don't copy!
+	
+	// there is no default constructor, and no copy-from-object
+
+	explicit array_wrapper(raw_array& o) : storage(o) {}
+	explicit array_wrapper(srk31::array<El, Dim>& obj) : storage(obj) {}
+	
+	// there *is* an assignment operator
+	array_wrapper& operator=(const wrapped_type& arg)
+	{
+		for (int i = 0; i < Dim; ++i) storage[i] = arg[i];
+		return *this;
+	}
+	
+	operator raw_array_ref() { return storage; }
+	operator raw_array_const_ref() const { return storage; }
+};
+
+// this is a helper function
+template <typename El, int Dim>
+srk31::array_wrapper<El, Dim> make_array_wrapper(El (&from)[Dim])
+{
+	return array_wrapper<El, Dim>(from);
+}
+
 } // end namespace srk31
 
 /* To compile this test code into an executable, use 
  * $(CXX) -x c++ -DMAKE_TEST_PROGRAM ... */
 #ifdef MAKE_TEST_PROGRAM
 #include <iostream>
-array<int, 3> function_returning_array()
+srk31::array<int, 3> function_returning_array()
 {
 	int blah[] = { 4, 5, 6 };
-	return array<int, 3>(blah);
+	return srk31::array<int, 3>(blah);
 }
 
 void function_taking_array(int args[3])
@@ -61,13 +103,20 @@ void function_taking_array(int args[3])
 
 int main(void)
 {
-	array<int, 42> my_arr;
+	srk31::array<int, 42> my_arr;
 	
 	// can we return an array?
 	auto triple = function_returning_array();
 	
 	// can we pass it to a function taking a vanilla array?
 	function_taking_array(triple);
+	
+	// can we assign to a local array? we need array_wrapper;
+	int my_triple[3];
+	srk31::make_array_wrapper(my_triple) = triple;
+
+	std::cout << "My new triple has been assigned: " 
+	<< my_triple[0] << ", " << my_triple[1] << ", " << my_triple[2] << std::endl;
 	
 	return 0;
 }
