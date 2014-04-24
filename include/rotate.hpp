88ea3646afee9bef3a5eb432b03b6dcbecaaccb8
Stephen Kell <srk31@cl.cam.ac.uk>
2014-04-24 14:52:26 +0100

    rotate.hpp: include guards and make functions inline.

--- include/rotate.hpp
+++ include/rotate.hpp
@@ -1,11 +1,16 @@
+#ifndef SRK31CXX_ROTATE_HPP_
+#define SRK31CXX_ROTATE_HPP_
+
 template <typename Integral> 
-Integral rotate_left(Integral val, unsigned n)
+inline Integral rotate_left(Integral val, unsigned n)
 {
 	return (val << n) | (val >> ((sizeof (Integral) * 8) - (signed) n));
 } 
 
 template <typename Integral> 
-Integral rotate_right(Integral val, unsigned n)
+inline Integral rotate_right(Integral val, unsigned n)
 {
 	return (val >> n) | (val << ((sizeof (Integral) * 8) - (signed) n));
 }
+
+#endif
