ab9315b5f518811b7d36b09317de66951550504e
Stephen Kell <srk31@cl.cam.ac.uk>
2015-06-19 17:04:18 +0100

    array.hpp: explicitly support zero-length arrays, and provide tail_t typedefs.

--- include/srk31/array.hpp
+++ include/srk31/array.hpp
@@ -1,6 +1,8 @@
 #ifndef LIBSRK31CXX_ARRAY_HPP_
 #define LIBSRK31CXX_ARRAY_HPP_
 
+#include <cstdlib>
+
 namespace srk31 {
 
 /* This is (perhaps) the thinnest possible wrapper around C-style arrays in C++, 
@@ -40,6 +42,37 @@ public:
 	//operator El* () { return &a[0]; }
 	//operator El const* () const { return &a[0]; }
 };
+template <typename El>
+class array<El, 0>
+{
+	// typedef El raw_array[];
+	// typedef raw_array& raw_array_ref;
+	// typedef raw_array const& raw_array_const_ref;
+public:
+	El& operator[](int ind) { if (ind != 0) abort(); return reinterpret_cast<El&>(this); }
+	const El& operator[](int ind) const { if (ind != 0) abort(); return reinterpret_cast<El&>(this); }
+	
+	// constructors
+	array(){}
+	array(const array<El, 0>& o) 
+	{
+	}
+	//array(const raw_array& o)
+	//{
+	//}
+	
+	//operator raw_array_ref() { return &(*this)[0]; }
+	//operator raw_array_const_ref() const { return &(*this)[0]; }
+};
+
+template <typename El, int Dim>
+class array_wrapper;
+
+/* We will define a tail() template for array_wrappers. */
+template <typename El, int Dim>
+inline
+array_wrapper<El, Dim - 1>
+tail(array_wrapper<El, Dim>);
 
 /* This is similar, but doesn't include any storage of its own. Rather,
  * it provides an interface around some preexisting array. It's necessary
@@ -74,14 +107,74 @@ public:
 	
 	operator raw_array_ref() { return storage; }
 	operator raw_array_const_ref() const { return storage; }
+	
+	template <typename El_, int Dim_>
+	friend
+	struct tail_t;
+};
+template <typename El>
+class array_wrapper<El, 0>
+{
+	typedef void wrapped_type;
+	
+	wrapped_type *p_storage;
+	
+	// typedef El raw_array[];
+	// typedef raw_array& raw_array_ref;
+	// typedef raw_array const& raw_array_const_ref;
+public:
+	El& operator[](int ind) { if (ind != 0) abort(); return *reinterpret_cast<El *>(p_storage); }
+	const El& operator[](int ind) const { if (ind != 0) abort(); return *reinterpret_cast<El *>(p_storage); }
+	
+	// constructors -- unlike array, these don't copy!
+	
+	// there is no default constructor, and no copy-from-object
+
+	template <typename T>
+	explicit array_wrapper(T& o) : p_storage(&o) {}
+	
+	explicit array_wrapper(srk31::array<El, 0>& obj) : p_storage(&obj) {}
+	
+	//operator raw_array_ref() { return storage; }
+	// operator raw_array_const_ref() const { return storage; }
 };
 
-// this is a helper function
+/* helper construct from array */
 template <typename El, int Dim>
 srk31::array_wrapper<El, Dim> make_array_wrapper(El (&from)[Dim])
 {
 	return array_wrapper<El, Dim>(from);
 }
+
+/* helper to make tail (suffix) array_wrapper */
+template <typename El, int Dim>
+struct tail_t
+{
+	static array_wrapper<El, Dim - 1>
+	get(array_wrapper<El, Dim> arg) 
+	{
+		typedef El tail_wrapped_type[Dim - 1];
+		return array_wrapper<El, Dim - 1>(*reinterpret_cast<tail_wrapped_type*>(&arg.storage[1]));
+	}
+};
+template <typename El>
+struct tail_t<El, 1>
+{
+	static array_wrapper<El, 0>
+	get(array_wrapper<El, 1> arg) 
+	{
+		return array_wrapper<El, 0>(arg.storage[1]);
+	}
+};
+
+template <typename El, int Dim>
+inline
+array_wrapper<El, Dim - 1>
+tail(array_wrapper<El, Dim> arg)
+{
+	return tail_t<El, Dim>::get(arg);
+}
+
 
 } // end namespace srk31
 
@@ -117,6 +210,11 @@ int main(void)
 
 	std::cout << "My new triple has been assigned: " 
 	<< my_triple[0] << ", " << my_triple[1] << ", " << my_triple[2] << std::endl;
+
+	// can we get the tail of an existing array?
+	srk31::array_wrapper<int, 3> triple_again(my_triple);
+	std::cout << "Tail begins: " << srk31::tail(triple_again)[0] << std::endl;
+	std::cout << "Tail continues: " << srk31::tail(srk31::tail(triple_again))[0] << std::endl;
 	
 	return 0;
 }
